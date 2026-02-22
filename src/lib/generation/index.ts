31795efb161b718ca4eef1339d1a8a865f035e81
Kurt Thorn <kurt.thorn@gmail.com>
2026-02-22 11:55:54 -0800

    feat: add generation module index

---
+++ src/lib/generation/index.ts
@@ -0,0 +1,4 @@
+export { generateWeapon, regenerateImage, rerollWeapon } from './orchestrator'
+export type { GenerateWeaponParams } from './orchestrator'
+export { runGenerationInBackground } from './background'
+export { withRetry, isTransientError } from './retry'
