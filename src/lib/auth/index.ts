f1bef1f6ffad7dc1f85c17bb981ce56c2b6c5223
Kurt Thorn <kurt.thorn@gmail.com>
2026-02-23 10:47:40 -0800

    feat: add auth module index

---
+++ src/lib/auth/index.ts
@@ -0,0 +1,3 @@
+export { verifyBasicAuth, createAuthChallengeHeaders } from './basic-auth'
+export type { AuthResult } from './basic-auth'
+export { createRateLimiter, rateLimiters } from './rate-limit'
